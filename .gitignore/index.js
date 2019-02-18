c9d7a276090cc4225a4ec073ecbae1b6d714ff8f
miss151001 <47727076+miss151001@users.noreply.github.com>
2019-02-18 18:17:22 +0100

    Delete index.js

--- .gitignore/index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-const bot = new Discord.Client({ disableEveryone: true });
-
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} est en ligne !`);
-});
-
-bot.login(NTQ1NzgyMjMwNzMyMTExODkz.D0uPSQ.tXh_Zx659fnk4kEuDM7t3GNeizI);
